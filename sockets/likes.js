cd836b5eb095bb35f4eb51d00a524e5a7ef9f06c
Carine Becker <caarine.m@gmail.com>
2021-08-12 19:02:13 -0300

    commitão na master

--- sockets/likes.js
+++ sockets/likes.js
@@ -1,5 +1,17 @@
 module.exports = (io) => {
   io.on('connection', (sockets) => {
     console.log('entrou');
+    let counter = {
+      feliz: 0,
+      triste: 0,
+      surpreso: 0,
+      indiferente: 0,
+    };
+    console.log(counter)
+    sockets.emit('sendCounter', { counter })
+
+    sockets.on('increaseCounter', (eventId) => {
+      console.log(eventId);
+    });
   });
-}
+};
