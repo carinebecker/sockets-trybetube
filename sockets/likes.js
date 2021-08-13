20caf3a5e1cafff03a51703e2a1beba0c336ec87
Carine Becker <caarine.m@gmail.com>
2021-08-13 16:01:47 -0300

    style done

--- sockets/likes.js
+++ sockets/likes.js
@@ -1,17 +1,18 @@
-module.exports = (io) => {
+let counter = {
+  feliz: 0,
+  triste: 0,
+  surpreso: 0,
+  indiferente: 0,
+};
+
+const likes = (io) => {
   io.on('connection', (sockets) => {
     console.log('entrou');
-    let counter = {
-      feliz: 0,
-      triste: 0,
-      surpreso: 0,
-      indiferente: 0,
-    };
-    console.log(counter)
     sockets.emit('sendCounter', { counter })
-
     sockets.on('increaseCounter', (eventId) => {
-      console.log(eventId);
+      counter = Object.assign(counter, eventId)
+      io.emit('sendCounter', { counter })
     });
   });
 };
+module.exports = likes;
